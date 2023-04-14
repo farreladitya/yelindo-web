4e10da1783e499b74e4e887323a71dece50ca5b8
Farrel Istihsan Aditya <farrelistihsanaditya@gmail.com>
2023-04-14 23:20:25 +0700

    header, produk mobile done

--- app/page.js
+++ app/page.js
@@ -1,124 +1,102 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import './globals.css'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+    <div className=' bg-neutral-800'>
+    <img src="https://media.discordapp.net/attachments/763335756403703839/1096415424708628540/foto_beranda_logo_baru.png?width=624&height=468" className="w-full" alt="Yelindo Logo" />
+    <div className=' text-gray-300 text-3xl font-bold mt-6 flex items-center justify-center'>
+      PT. YELINDO SURYA JAYA
+    </div>
+    <div className=' text-gray-300 text-lg font-semibold mt-2 flex items-center justify-center'>
+      PEDAGANG BESAR ALAT KESEHATAN
+    </div>
+    <div className=' text-gray-400 text-base font-semibold mt-2 flex items-center justify-center'>
+      Jl. Jambangan Kebon Agung No. 11 Surabaya
+    </div>
+    <div class="mt-3 flex items-center justify-center md:mb-0">
+              <a href="https://yelindo.id/" className="mb-8 bg-green-700 hover:bg-green-600 text-white font-bold py-4 px-4 rounded-lg inline-flex">
+              <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+          <span class="text-white ">Hubungi Kami</span>
+              </a>
+          </div>
+    </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <div>
+      <div className='bg-gray-100 text-[#5956E9] font-bold text-4xl p-8 flex items-center justify-center'>Produk</div>
+    <div class="bg-gray-100 px-8 gap-4 grid grid-cols-2 md:grid-cols-3 md:gap-20">
+<div class=" max-w-md rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer flex flex-col justify-center items-center">
+  <div>
+    <img src="https://media.ldlc.com/r1600/ld/products/00/05/82/02/LD0005820208_1.jpg" alt="" />
+  </div>
+  <div class="bg-white flex flex-col items-center justify-center">
+  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 md:text-3xl">Medikasa Kasa Hidrophile</h3>  
+  </div>
+</div>
+<div class="max-w-md  rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer flex flex-col justify-center items-center">
+  <div>
+    <img src="https://media.ldlc.com/r1600/ld/products/00/05/82/02/LD0005820208_1.jpg" alt="" />
+  </div>
+  <div class=" bg-white flex flex-col items-center justify-center">
+  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 md:text-3xl">Medikasa Kasa Hidrophile</h3>  
+  </div>
+</div>
+<div class="max-w-md  rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer flex flex-col justify-center items-center">
+  <div>
+    <img src="https://media.ldlc.com/r1600/ld/products/00/05/82/02/LD0005820208_1.jpg" alt="" />
+  </div>
+  <div class=" bg-white flex flex-col items-center justify-center">
+  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 md:text-3xl">Medikasa Kasa Hidrophile</h3>  
+  </div>
+</div>
+<div class="max-w-md  rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer flex flex-col justify-center items-center">
+  <div>
+    <img src="https://media.ldlc.com/r1600/ld/products/00/05/82/02/LD0005820208_1.jpg" alt="" />
+  </div>
+  <div class=" bg-white flex flex-col items-center justify-center">
+  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 md:text-3xl">Medikasa Kasa Hidrophile</h3>  
+  </div>
+</div>
+<div class="max-w-md rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer flex flex-col justify-center items-center">
+  <div>
+    <img src="https://media.ldlc.com/r1600/ld/products/00/05/82/02/LD0005820208_1.jpg" alt="" />
+  </div>
+  <div class=" bg-white flex flex-col items-center justify-center">
+  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 md:text-3xl">Medikasa Kasa Hidrophile</h3>  
+  </div>
+</div>
+<div class="max-w-md rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer flex flex-col justify-center items-center">
+  <div>
+    <img src="https://media.ldlc.com/r1600/ld/products/00/05/82/02/LD0005820208_1.jpg" alt="" />
+  </div>
+  <div class=" bg-white flex flex-col items-center justify-center">
+  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 md:text-3xl">Medikasa Kasa Hidrophile</h3>  
+  </div>
+</div>
+  </div>
+  <a href='#'><h3 className='text-right mt-7 mr-5 text-[#5956E9] font-bold text-sm md:text-lg '>Lihat Lebih Banyak...</h3></a>
+  <div>
+  <div className=' text-[#5956E9] font-bold text-4xl p-8 flex items-center justify-center'>Tentang Kami</div>
+  <div class="mb-1 flex  items-center justify-center p-4">
+  <div class="flex items-center justify-center md:w-3/4">
+  <img src="https://media.discordapp.net/attachments/763335756403703839/1096388080556064778/logo_yel_baru.png?width=494&height=468" class="h-24 mr-3 md:h-36" alt="Yelindo Logo" />
+  <div class="flex flex-col md:items-center md:justify-center">
+    <span class="text-3xl md:self-start md:mt-1 md:text-5xl font-bold">PT. YELINDO SURYA JAYA</span> 
+    <h3 class="text-gray-600 max-[637px]:hidden sm:block md:self-center text-justify">Perusahaan yang bergerak di bidang perdagangan alat kesehatan tercatat pada akte pendirian perseroan terbatas pada tanggal 26 November 2020 dengan alamat Jl.Jambangan Kebon Agung Nomor 11 Surabaya, dengan personil perusahaan yang cukup berpengalaman lebih dari 20 tahun dibidangnya. Mempunyai visi misi sebagai berikut :</h3>
+  </div>
+</div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <div className='text-gray-600  font-normal text-base px-14 text-justify min-[637px]:hidden'>
+          Perusahaan yang bergerak di bidang perdagangan alat kesehatan tercatat pada akte pendirian perseroan terbatas pada tanggal 26 November 2020 dengan alamat Jl.Jambangan Kebon Agung Nomor 11 Surabaya, dengan personil perusahaan yang cukup berpengalaman lebih dari 20 tahun dibidangnya. 
+Mempunyai visi misi sebagai berikut :
+              </div>
+  </div>
+  </div>
+  </div>
+   
   )
 }
