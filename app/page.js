2759cd3ee99b6d2e29b86bae7b9abef6f44aa9d0
Farrel Istihsan Aditya <farrelistihsanaditya@gmail.com>
2023-04-15 12:59:24 +0700

    responsive header & tentang kami done

--- app/page.js
+++ app/page.js
@@ -3,24 +3,37 @@ import './globals.css'
 export default function Home() {
   return (
     <div>
-    <div className=' bg-neutral-800'>
-    <img src="https://media.discordapp.net/attachments/763335756403703839/1096415424708628540/foto_beranda_logo_baru.png?width=624&height=468" className="w-full" alt="Yelindo Logo" />
-    <div className=' text-gray-300 text-3xl font-bold mt-6 flex items-center justify-center'>
-      PT. YELINDO SURYA JAYA
+<div class="flex flex-col md:flex-row bg-neutral-800">
+  <div class="md:w-1/2 md:order-2">
+    <img src="https://media.discordapp.net/attachments/763335756403703839/1096415424708628540/foto_beranda_logo_baru.png?width=624&height=468" class="w-full" alt="Yelindo Logo" />
+  </div>
+ 
+  <div class="md:w-1/2 px-4 py-8 md:py-0 md:order-1">
+    <div className='md:p-6 md:mt-20 '>
+    <div class="text-gray-300 text-3xl md:text-4xl font-bold mb-4 flex items-center justify-center md:justify-start">PT. YELINDO SURYA JAYA</div>
+    <div class="text-gray-300 text-lg md:text-2xl font-semibold mb-1 flex items-center justify-center md:justify-start">PEDAGANG BESAR ALAT KESEHATAN</div>
+    <div class="text-gray-400 text-base md:text-lg font-semibold mb-6 flex items-center justify-center md:justify-start">Jl. Jambangan Kebon Agung No. 11 Surabaya</div>
+    <div class="flex items-center justify-center md:justify-start">
+      <a href="https://yelindo.id/" class="bg-green-700 hover:bg-green-600 text-white font-bold py-4 px-6 rounded-lg inline-flex items-center">
+      <svg className='w-6 md:w-8' viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M16 31C23.732 31 30 24.732 30 17C30 9.26801 23.732 3 16 3C8.26801 3 2 9.26801 2 17C2 19.5109 2.661 21.8674 3.81847 23.905L2 31L9.31486 29.3038C11.3014 30.3854 13.5789 31 16 31ZM16 28.8462C22.5425 28.8462 27.8462 23.5425 27.8462 17C27.8462 10.4576 22.5425 5.15385 16 5.15385C9.45755 5.15385 4.15385 10.4576 4.15385 17C4.15385 19.5261 4.9445 21.8675 6.29184 23.7902L5.23077 27.7692L9.27993 26.7569C11.1894 28.0746 13.5046 28.8462 16 28.8462Z" fill="#BFC8D0"/>
+<path d="M28 16C28 22.6274 22.6274 28 16 28C13.4722 28 11.1269 27.2184 9.19266 25.8837L5.09091 26.9091L6.16576 22.8784C4.80092 20.9307 4 18.5589 4 16C4 9.37258 9.37258 4 16 4C22.6274 4 28 9.37258 28 16Z" fill="url(#paint0_linear_87_7264)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M16 30C23.732 30 30 23.732 30 16C30 8.26801 23.732 2 16 2C8.26801 2 2 8.26801 2 16C2 18.5109 2.661 20.8674 3.81847 22.905L2 30L9.31486 28.3038C11.3014 29.3854 13.5789 30 16 30ZM16 27.8462C22.5425 27.8462 27.8462 22.5425 27.8462 16C27.8462 9.45755 22.5425 4.15385 16 4.15385C9.45755 4.15385 4.15385 9.45755 4.15385 16C4.15385 18.5261 4.9445 20.8675 6.29184 22.7902L5.23077 26.7692L9.27993 25.7569C11.1894 27.0746 13.5046 27.8462 16 27.8462Z" fill="white"/>
+<path d="M12.5 9.49989C12.1672 8.83131 11.6565 8.8905 11.1407 8.8905C10.2188 8.8905 8.78125 9.99478 8.78125 12.05C8.78125 13.7343 9.52345 15.578 12.0244 18.3361C14.438 20.9979 17.6094 22.3748 20.2422 22.3279C22.875 22.2811 23.4167 20.0154 23.4167 19.2503C23.4167 18.9112 23.2062 18.742 23.0613 18.696C22.1641 18.2654 20.5093 17.4631 20.1328 17.3124C19.7563 17.1617 19.5597 17.3656 19.4375 17.4765C19.0961 17.8018 18.4193 18.7608 18.1875 18.9765C17.9558 19.1922 17.6103 19.083 17.4665 19.0015C16.9374 18.7892 15.5029 18.1511 14.3595 17.0426C12.9453 15.6718 12.8623 15.2001 12.5959 14.7803C12.3828 14.4444 12.5392 14.2384 12.6172 14.1483C12.9219 13.7968 13.3426 13.254 13.5313 12.9843C13.7199 12.7145 13.5702 12.305 13.4803 12.05C13.0938 10.953 12.7663 10.0347 12.5 9.49989Z" fill="white"/>
+<defs>
+<linearGradient id="paint0_linear_87_7264" x1="26.5" y1="7" x2="4" y2="28" gradientUnits="userSpaceOnUse">
+<stop stop-color="#5BD066"/>
+<stop offset="1" stop-color="#27B43E"/>
+</linearGradient>
+</defs>
+</svg>        
+<span class="text-white ml-1">Hubungi Kami</span>
+      </a>
     </div>
-    <div className=' text-gray-300 text-lg font-semibold mt-2 flex items-center justify-center'>
-      PEDAGANG BESAR ALAT KESEHATAN
     </div>
-    <div className=' text-gray-400 text-base font-semibold mt-2 flex items-center justify-center'>
-      Jl. Jambangan Kebon Agung No. 11 Surabaya
-    </div>
-    <div class="mt-3 flex items-center justify-center md:mb-0">
-              <a href="https://yelindo.id/" className="mb-8 bg-green-700 hover:bg-green-600 text-white font-bold py-4 px-4 rounded-lg inline-flex">
-              <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-          <span class="text-white ">Hubungi Kami</span>
-              </a>
-          </div>
-    </div>
+  </div>
+  
+</div>
 
     <div>
       <div className='bg-gray-100 text-[#5956E9] font-bold text-4xl p-8 flex items-center justify-center'>Produk</div>
@@ -30,7 +43,7 @@ export default function Home() {
     <img src="https://media.ldlc.com/r1600/ld/products/00/05/82/02/LD0005820208_1.jpg" alt="" />
   </div>
   <div class="bg-white flex flex-col items-center justify-center">
-  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 md:text-3xl">Medikasa Kasa Hidrophile</h3>  
+  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 min-[551px]:text-2xl min-[551px]:text-2xl md:text-3xl">Medikasa Kasa Hidrophile</h3>  
   </div>
 </div>
 <div class="max-w-md  rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer flex flex-col justify-center items-center">
@@ -38,7 +51,7 @@ export default function Home() {
     <img src="https://media.ldlc.com/r1600/ld/products/00/05/82/02/LD0005820208_1.jpg" alt="" />
   </div>
   <div class=" bg-white flex flex-col items-center justify-center">
-  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 md:text-3xl">Medikasa Kasa Hidrophile</h3>  
+  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 min-[551px]:text-2xl md:text-3xl">Medikasa Kasa Hidrophile</h3>  
   </div>
 </div>
 <div class="max-w-md  rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer flex flex-col justify-center items-center">
@@ -46,7 +59,7 @@ export default function Home() {
     <img src="https://media.ldlc.com/r1600/ld/products/00/05/82/02/LD0005820208_1.jpg" alt="" />
   </div>
   <div class=" bg-white flex flex-col items-center justify-center">
-  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 md:text-3xl">Medikasa Kasa Hidrophile</h3>  
+  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 min-[551px]:text-2xl md:text-3xl">Medikasa Kasa Hidrophile</h3>  
   </div>
 </div>
 <div class="max-w-md  rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer flex flex-col justify-center items-center">
@@ -54,7 +67,7 @@ export default function Home() {
     <img src="https://media.ldlc.com/r1600/ld/products/00/05/82/02/LD0005820208_1.jpg" alt="" />
   </div>
   <div class=" bg-white flex flex-col items-center justify-center">
-  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 md:text-3xl">Medikasa Kasa Hidrophile</h3>  
+  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 min-[551px]:text-2xl md:text-3xl">Medikasa Kasa Hidrophile</h3>  
   </div>
 </div>
 <div class="max-w-md rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer flex flex-col justify-center items-center">
@@ -62,7 +75,7 @@ export default function Home() {
     <img src="https://media.ldlc.com/r1600/ld/products/00/05/82/02/LD0005820208_1.jpg" alt="" />
   </div>
   <div class=" bg-white flex flex-col items-center justify-center">
-  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 md:text-3xl">Medikasa Kasa Hidrophile</h3>  
+  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 min-[551px]:text-2xl md:text-3xl">Medikasa Kasa Hidrophile</h3>  
   </div>
 </div>
 <div class="max-w-md rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer flex flex-col justify-center items-center">
@@ -70,11 +83,13 @@ export default function Home() {
     <img src="https://media.ldlc.com/r1600/ld/products/00/05/82/02/LD0005820208_1.jpg" alt="" />
   </div>
   <div class=" bg-white flex flex-col items-center justify-center">
-  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 md:text-3xl">Medikasa Kasa Hidrophile</h3>  
+  <h3 class="text-xl font-semibold text-black text-center mb-4 md:p-4 min-[551px]:text-2xl md:text-3xl">Medikasa Kasa Hidrophile</h3>  
   </div>
 </div>
   </div>
   <a href='#'><h3 className='text-right mt-7 mr-5 text-[#5956E9] font-bold text-sm md:text-lg '>Lihat Lebih Banyak...</h3></a>
+
+  {/* Tentang Kami */}
   <div>
   <div className=' text-[#5956E9] font-bold text-4xl p-8 flex items-center justify-center'>Tentang Kami</div>
   <div class="mb-1 flex  items-center justify-center p-4">
@@ -82,18 +97,44 @@ export default function Home() {
   <img src="https://media.discordapp.net/attachments/763335756403703839/1096388080556064778/logo_yel_baru.png?width=494&height=468" class="h-24 mr-3 md:h-36" alt="Yelindo Logo" />
   <div class="flex flex-col md:items-center md:justify-center">
     <span class="text-3xl md:self-start md:mt-1 md:text-5xl font-bold">PT. YELINDO SURYA JAYA</span> 
-    <h3 class="text-gray-600 max-[637px]:hidden sm:block md:self-center text-justify">Perusahaan yang bergerak di bidang perdagangan alat kesehatan tercatat pada akte pendirian perseroan terbatas pada tanggal 26 November 2020 dengan alamat Jl.Jambangan Kebon Agung Nomor 11 Surabaya, dengan personil perusahaan yang cukup berpengalaman lebih dari 20 tahun dibidangnya. Mempunyai visi misi sebagai berikut :</h3>
+    <h3 class="text-gray-600 max-[637px]:hidden md:self-center text-justify md:text-xl">Perusahaan yang bergerak di bidang perdagangan alat kesehatan tercatat pada akte pendirian perseroan terbatas pada tanggal 26 November 2020 dengan alamat Jl.Jambangan Kebon Agung Nomor 11 Surabaya, dengan personil perusahaan yang cukup berpengalaman lebih dari 20 tahun dibidangnya. Mempunyai visi misi sebagai berikut :</h3>
+  </div>
+</div>
+</div>
+      <div className='text-gray-600  font-normal text-lg px-14 text-justify min-[637px]:hidden'>
+          Perusahaan yang bergerak di bidang perdagangan alat kesehatan tercatat pada akte pendirian perseroan terbatas pada tanggal 26 November 2020 dengan alamat Jl.Jambangan Kebon Agung Nomor 11 Surabaya, dengan personil perusahaan yang cukup berpengalaman lebih dari 20 tahun dibidangnya. Mempunyai visi misi sebagai berikut :
+      </div>
+      <div class="flex flex-wrap items-center justify-center md:items-start ">
+  <div class="text-left md:w-1/2 flex flex-col justify-center items-center">
+    <img src="https://cdn.discordapp.com/attachments/763335756403703839/1096473263858782398/VISI.png" class="py-6 mt-0 " alt="Visi" />
+    <div class="text-gray-600 font-semibold text-lg px-14">
+      MENJADI DISTRIBUTOR YANG HANDAL, TERPERCAYA, TERBAIK DAN TERDEPAN DALAM MELAYANI DAN MENDISTRIBUSIKAN KEBUTUHAN ALAT KESEHATAN DI MASYARAKAT DENGAN FOKUS PADA PELAYANAN DAN KEPUASAN PELANGGAN
+    </div>
+  </div>
+  <div class="text-left md:w-1/2 flex flex-col justify-center items-center">
+    <img src="https://media.discordapp.net/attachments/763335756403703839/1096473263196086292/MISI.png" class="py-6 " alt="Misi" />
+    <div class="text-gray-600 font-semibold text-lg px-14">
+      <ul class="list-disc">
+        <li class="list-item">MENYEDIAKAN SEGALA KEBUTUHAN ALAT KESEHATAN YANG BERKUALITAS SERTA MENDISTRIBUSIKANNYA SECARA CEPAT DAN EFISIEN KE SELURUH DAERAH</li>
+        <li class="list-item">KOMPETITIF DAN BERDAYA SAING TINGGI</li>
+        <li class="list-item">LAYANAN PRIMA YANG MENGUTAMAKAN KEPUASAN PELANGGAN</li>
+      </ul>
+    </div>
   </div>
 </div>
 
-</div>
 
+  </div>
 
-
-          <div className='text-gray-600  font-normal text-base px-14 text-justify min-[637px]:hidden'>
-          Perusahaan yang bergerak di bidang perdagangan alat kesehatan tercatat pada akte pendirian perseroan terbatas pada tanggal 26 November 2020 dengan alamat Jl.Jambangan Kebon Agung Nomor 11 Surabaya, dengan personil perusahaan yang cukup berpengalaman lebih dari 20 tahun dibidangnya. 
-Mempunyai visi misi sebagai berikut :
-              </div>
+  {/* Profil Kami */}
+  <div>
+  <svg className='mt-6 w-screen h-[26px]'  viewBox="0 0 1440 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="-7" width="1453" height="26" fill="#5956E9"/>
+</svg>
+<svg className=' md:mt-3 w-screen h-[26px]'  viewBox="0 0 1440 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="-7" width="1453" height="26" fill="#868686"/>
+</svg>
+  <div className=' text-[#5956E9] font-bold text-4xl p-8 flex items-center justify-center'>Profil Kami</div>
   </div>
   </div>
   </div>
